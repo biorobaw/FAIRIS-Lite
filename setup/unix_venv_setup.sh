ee857ef77464cc7c6d495be6d768651117ac847d
Chance Hamilton <chamilton4@usf.edu>
2024-08-30 15:35:17 -0400

    removed old setup process

--- setup/unix_venv_setup.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-venv/bin/python3 -m pip install numpy scikit-learn scipy matplotlib astar pandas
--- setup/windows_venv_setup.bat
+++
@@ -1,3 +0,0 @@
-
-
-.\venv\Scripts\python.exe -m pip install numpy scikit-learn scipy matplotlib astar pandas
